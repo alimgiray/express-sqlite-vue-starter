0dba3347f5d0c725dbd9902dfb6bff504dce1ace
Alim Giray Aytar <alimgiray@gmail.com>
2021-12-28 20:40:40 +0300

    update tailwind to v3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
